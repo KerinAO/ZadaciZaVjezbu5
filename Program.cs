2bb8f1c676013cef427e0ca0864043d5c2a7c1cf
KerinAO <90246117+KerinAO@users.noreply.github.com>
2022-10-17 00:12:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Search ObjektSearch = new Search();
+
+            int[] arrSearch = new int[100];
+            int neparniBroj = 1;
+
+            for (int i = 0; i < 100; i++)
+            {
+                arrSearch[i] = neparniBroj;
+                neparniBroj += 2;
+            }
+
+            Console.WriteLine("Index broja 75 je:" + ObjektSearch.BinarySearch(arrSearch, 75));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Search.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadaciZaVjezbu5
+{
+    internal class Search
+    {
+        public int BinarySearch(int[] arr, int x)
+        {
+            int i = 0, r = arr.Length - 1;
+
+            while (i <= r)
+            {
+                int m = i + (r - i) / 2;
+                if (arr[m] == x)
+                    return m;
+                if (arr[m] < x)
+                    i = m + 1;
+                else
+                    r = m - 1;
+            }
+            return -1;
+        }
+    }
+}
